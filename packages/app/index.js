ae6c362c01af225b2551a1ca515f150bfd6ee24c
Alex Batalov <radist2s@gmail.com>
2023-01-16 04:33:41 +0400

    added comments

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,3 +1,4 @@
 export const app = () => {
+  // comment app 1
   return 'Hello world! Yes!'
 }
--- packages/site/index.js
+++ packages/site/index.js
@@ -1,3 +1,3 @@
 import {app} from "@radist2s/app"
-// added some comment 4
+// added some comment 5
 console.log('Site', app())
