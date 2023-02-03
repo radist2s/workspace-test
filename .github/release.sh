c1b6948aa7a3b5b10681163788d6bf147063dc25
Alex Batalov <radist2s@gmail.com>
2023-02-03 07:28:29 +0400

    docs(changeset): change

--- .github/release.sh
+++ .github/release.sh
@@ -3,4 +3,4 @@ set -o errexit
 
 echo "//npm.pkg.github.com/:_authToken=$1" > $HOME/.npmrc
 yarn release
-git checkout HEAD -- .npmrc &> /dev/null || rm .npmrc &> /dev/null || true
+git checkout HEAD -- .npmrc &>> /dev/null || rm .npmrc &>> /dev/null || true
