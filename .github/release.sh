517d41e22fcbdf39a0391a0fb658bafc688c6c56
Alex Batalov <radist2s@gmail.com>
2023-02-10 00:47:55 +0400

    added

--- .github/release.sh
+++ .github/release.sh
@@ -5,4 +5,4 @@ echo "//npm.pkg.github.com/:_authToken=$1" > $HOME/.npmrc
 yarn workspace @radist2s/app run build
 yarn workspace @radist2s/app run build:paths-dependencies
 changeset publish
-git checkout HEAD -- .npmrc &>> /dev/null || rm .npmrc &>> /dev/null || true
+(git checkout HEAD -- .npmrc 2> /dev/null) || (rm .npmrc 2> /dev/null) || true
